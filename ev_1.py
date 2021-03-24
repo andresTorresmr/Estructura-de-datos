c1250023246230cf1057f83128974e1f28685840
andresTorresmr <70896096+andresTorresmr@users.noreply.github.com>
2021-03-24 13:18:36 -0600

    Add files via upload
    Código de la evidencia #1

---
+++ ev_1.py
@@ -0,0 +1,79 @@
+#Registro del detalle de ventas un negocio de ferretería
+#Requerimientos funcionales:
+#Se debe ofrecer un menú navegable con las siguientes opciones: Registrar una venta Consultar una venta Salir
+import os
+LimpiarPantalla = lambda: os.system('cls')
+SEPARADOR = ("*" * 20) + "\n"
+totales={}
+
+#Elaborado por Andrés Torres Montemayor
+def cargar():
+    productos={}
+    cuenta=[]
+    total=[]
+    monto_total = 0
+    if productos:
+        clave = max(productos)+1
+    else:
+        clave = 1
+    while True:
+        descripcion=input("Ingrese la descripcion:")
+        if descripcion == "":
+            break
+        else:
+            unidades_venta=int(input("Unidades vendidas:"))
+            precio=float(input("Ingrese el precio de venta:"))
+            cuenta.append((descripcion,unidades_venta,precio))
+            suma = (precio*unidades_venta)
+            monto_total = suma + monto_total
+    total.append(monto_total)
+    totales[clave]=total
+    productos[clave]=cuenta
+
+    return productos
+
+#Elaborado por Andrés Torres Montemayor
+def consulta(productos):
+    if productos:
+        buscar=int(input("Ingrese la clave de venta a consultar:"))
+        if buscar in productos.keys():
+            for articulo,unidades,precio in productos[buscar]:
+                print("Articulo: ",articulo,"\nUnidades vendidas: ",unidades,"\nPrecio por unidad: ",precio)
+            for total in totales[buscar]:
+                print("El total de la cuenta fue de: $",total)
+        else:
+            print("No se encontro la venta deseada.")
+    else:
+        print("No existen ventas registradas")
+
+#Elaborado por Javier Alejandro Álvarez Rios
+def menu():
+    while (True):
+            LimpiarPantalla()
+            print (SEPARADOR)
+            print("FERRETERIA\n")
+            print (SEPARADOR)
+            print("[1] Registrar una venta.")
+            print("[2] Consultar una venta.")
+            print("[3] Salir.")
+            opcion = int(input("Selecciona una opción  > "))
+            if opcion <= 3:
+                if opcion==1:
+                    productos=cargar()
+
+                if opcion==2:
+                    consulta(productos)
+
+                if opcion==3:
+                    print("Gracias por utilizar el programa.")
+                    break
+
+                input("Pulsa enter para contunuar...")
+            else:
+                print("Esa respuesta no es válida.")
+                input("Pulsa enter para continuar...")
+
+
+menu()
+
+
